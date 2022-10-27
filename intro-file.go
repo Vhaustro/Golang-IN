5d95c69263a8a33225e54090978b7377b9d26fcc
unknown <mandenno228@gmail.com>
2022-10-27 23:37:29 +0300

    added files

---
+++ intro-file.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World! How are you?")
+}
+
+//VARIABLES
+//A variable is a method of storing information
+//It can be changed/modified but it's just a slot where we place information.
+//In a variable the right side is known as the "TYPE"
+//Once a variable is declared to have a specific type, it cannot change.
+//In a variable there can only be numbers, letters and underscores. It follows the
+
+//Types
+//uint8/byte (0=255)(Unsigned Interger) (Positive) (One byte = 8 bits)
+//Bit - 0 or 1's to represent information (BASIC)
+//Signed integers can be both positive and negative
